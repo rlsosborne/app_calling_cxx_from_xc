3ba061ab829cc6421daa003fec6831ae7427187c
Richard Osborne <richard@xmos.com>
2013-06-19 17:08:08 +0200

    Update file2.h

--- file2.h
+++ file2.h
@@ -1,6 +1,6 @@
 #ifndef file2_h
 #define file2_h
 
-// Put prototypes of functions / classes / types that only make sense in C++.
+// Put prototypes of functions / classes / types that can't be called from C / XC here.
 
 #endif //file2_h
